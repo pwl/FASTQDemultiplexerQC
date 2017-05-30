925322ece0be3ace4ef224ee5b616ef2a74aad7d
Paweł Biernat <pawel.biernat+github@gmail.com>
2017-05-30 19:57:13 +0200

    First working version

--- src/FASTQDemultiplexerQC.jl
+++ src/FASTQDemultiplexerQC.jl
@@ -1,74 +1,97 @@
 module FASTQDemultiplexerQC
 
 
-import FASTQDemultiplexer: Output, Interpreter
+import FASTQDemultiplexer: Output, Protocol, Barcode, InterpretedRecord,
+    mergeoutput
 using DataFrames
 using Weave
 
 
-type OutputQC <: Output
+type OutputQC{N} <: Output
     results::DataFrame
-    cellbclen::UInt
-    umibclen::UInt
-    selectedcells::Vector{UInt}
+    poly::NTuple{N,DataFrame}
+    nreads::Float64
+    countpoly::Bool
 end
 
 
-function OutputQC(protocol::Interpreter;
-                  outputdir::String = ".",
-                  selectedcells::String = "",
-                  kwargs...)
-    bcodes = genselectedcells(selectedcells)
-    cellbclen = sum(length(pos) for pos in protocol.cellpos)
-    umibclen  = sum(length(pos) for pos in protocol.umipos)
-
-    return OutputQC([],[],cellbclen,umibclen,bcodes)
-end
-
-
-function Base.write(o::OutputQC,ir::InterpretedRecord)
-    push!(o.cellid,ir.cellid)
-    push!(o.umiid,ir.umiid)
-end
-
-
-function mergeoutput(outputs::Vector{OutputQC};
+function OutputQC{N}(protocol::Protocol{N};
                      outputdir::String = ".",
+                     maxreads = Inf,
+                     countpoly::Bool = true,
                      kwargs...)
 
-    o1 = outputs[1]
-    args = Dict{String,Any}()
-    args["counts"], args["cellids"], args["umiids"] = counttable(outputs)
-    args["cellnames"] = map(i->idtoname(i,o1.cellbclen),args["cellids"])
-    args["uminames"]  = map(i->idtoname(i,o1.umibclen), args["umiids"])
+    results = DataFrame(cellid = UInt[],
+                        umiid = UInt[],
+                        groupname = PooledDataArray(String,UInt8,0))
+    poly = [
+        DataFrame(A=UInt8[],C=UInt8[],T=UInt8[],G=UInt8[])
+        for i in 1:N ]
+    return OutputQC(results,(poly...),Float64(maxreads),countpoly)
+end
 
-    weave(Pkg.dir("FASTQDemultiplexer","src","output","QC","counts.jmd"),
+
+function Base.write{N}(o::OutputQC{N},ir::InterpretedRecord{N})
+
+    if o.nreads <= 0
+        return
+    else
+        o.nreads-=1
+    end
+
+    push!(o.results,(ir.cellid.val,ir.umiid.val,ir.groupname))
+    if o.countpoly
+        for i = 1:N
+            seq = ir.records[i].data[ir.records[i].sequence]
+            push!(o.poly[i],
+                  map(UInt8['A','C','T','G']) do nuc
+                  longeststreak(seq,nuc)
+                  end)
+        end
+    end
+end
+
+
+function mergeoutput{N}(outputs::Vector{OutputQC{N}};
+                        outputdir::String = ".",
+                        kwargs...)
+    if length(outputs) > 1
+        results = vcat((o.results for o in outputs)...)
+        poly = map(1:N) do i
+            vcat((o.poly[i] for o in outputs)...)
+        end
+    else
+        results = outputs[1].results
+        poly = outputs[1].poly
+    end
+
+    mkpath(outputdir)
+
+    report(results,poly,outputdir)
+end
+
+function report(results, poly, outputdir)
+
+    args = Dict(:results=>results,
+                :poly=>poly)
+
+    weave(Pkg.dir("FASTQDemultiplexerQC","src","weave","QC.jmd"),
           args = args, out_path = outputdir)
 end
 
 
-function counttable(outputs::Vector{OutputQC})
-    #1) merge the encountered cells and umis
-    cellids = Set{UInt}()
-    umiids = Set{UInt}()
-    @time for o in outputs
-        union!(cellids,o.cellid)
-        union!(umiids,o.umiid)
-    end
-    delete!(cellids,0)
-    delete!(umiids,0)
-    @time cellind = Dict(id=>i for (i,id) in enumerate(cellids))
-    @time umiind  = Dict(id=>i for (i,id) in enumerate(umiids))
-
-    @time counts = zeros(UInt,(length(cellids),length(umiids)))
-    @time for o in outputs
-        for (cellid, umiid) in zip(o.cellid, o.umiid)
-            if cellid > 0 && umiid > 0
-                counts[cellind[cellid],umiind[umiid]] += 1
-            end
+function longeststreak(seq::Vector{UInt8},nuc::UInt8)
+    len=0
+    current=0
+    for a in seq
+        if a==nuc
+            len=max(len,current)
+            current+=1
+        else
+            current=0
         end
     end
-    counts, collect(cellids), collect(umiids)
+    return len
 end
 
 end # module
